c627c8df57d79a57e9dd7e59a3c6417f8e41d5ed
Sam Rowe <sgrowe@live.co.uk>
2025-04-23 13:09:03 +0100

    Add VS Code keybindings.json

--- fish/functions/setup_symlinks.fish
+++ fish/functions/setup_symlinks.fish
@@ -3,9 +3,13 @@ function setup_symlinks
     set -l fish_trace 1
 
     set -l vsc_settings "$HOME/Library/Application Support/Code/User/settings.json"
+    set -l vsc_keymap "$HOME/Library/Application Support/Code/User/keybindings.json"
 
-    # VS Code settings
+    # VS Code
     if not test -e $vsc_settings
         ln -s ~/.config/sam_misc/vscode/settings.json $vsc_settings
+    end
+    if not test -e $vsc_keymap
+        ln -s ~/.config/sam_misc/vscode/keybindings.json $vsc_keymap
     end
 end
