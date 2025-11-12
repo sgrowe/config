8b96262c116d78d79bc9e55eb8d3428313395378
Sam Rowe <sgrowe@live.co.uk>
2025-11-12 15:30:48 +0000

    Speed up `dev_migrations` a bit

--- fish/functions/dev_migrations.fish
+++ fish/functions/dev_migrations.fish
@@ -1,6 +1,4 @@
 function dev_migrations --description 'Prints out migration files on the current branch but not on master'
-    if jj root --ignore-working-copy >/dev/null 2>&1
-        jj diff --ignore-working-copy -r 'present(master)::@' --quiet --name-only 'glob:app/server/migrations/**' | rg "app/server/migrations/(\d+.+)" --replace '$1' | xargs echo -n
-        or true
-    end
+    jj diff --ignore-working-copy -r 'present(master)::@' --quiet --name-only 'glob:app/server/migrations/**' 2>/dev/null | rg "app/server/migrations/(\d+.+)" --replace '$1' | string collect
+    or true
 end
