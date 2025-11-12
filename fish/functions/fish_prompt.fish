fa6d2c9c73fd358128734e404dc2e08f9495c9ba
Sam Rowe <sgrowe@live.co.uk>
2025-11-12 15:09:49 +0000

    Show current dev migrations in shell prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -26,6 +26,8 @@ function fish_prompt --description 'Write out the prompt'
     set -l status_color (set_color $fish_color_status)
     set -l statusb_color (set_color $bold_flag $fish_color_status)
     set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+    set -l migrations_color (set_color $fish_color_autosuggestion)
+    set -l migrations (dev_migrations)
 
-    echo -n -s (prompt_login)' ' (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
+    echo -n -s (set_color $color_cwd) (prompt_pwd) $normal (fish_vcs_prompt) $migrations_color " "$migrations $normal " "$prompt_status $suffix " "
 end
